0c3366381b51b863bb7f83a1872b6f30a7f74436
Ed Clements <ed@bookingbug.com>
2019-05-08 18:06:46 +0100

    Get spotify urls

--- fix_music_picks.py
+++ fix_music_picks.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
+import sys
 import yaml
 import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+import time
 
-sp = spotipy.Spotify()
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 releases = yaml.load(open('_data/music-picks/2019.yml', 'r'))
 for release in releases:
     if 'spotify' in release:
         print(release['artist'])
         print(release['title'])
-        print('\n')
-        results = sp.search(q=release['title'], limit=20)
-        print(results)
+        results = sp.search(q=release['title'],type='album', limit=20)
+        print(results['albums']['items'][0]['external_urls']['spotify'])
         print('\n')
         time.sleep(0.5)
