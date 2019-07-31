b49fc42b868bcc3bfa0b64577957af7782b7fe9a
Ed Clements <ed@bookingbug.com>
2019-07-31 17:40:09 +0100

    Spotify links

--- fix_music_picks.py
+++ fix_music_picks.py
@@ -10,12 +10,21 @@ import time
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-releases = yaml.load(open('_data/music-picks/2019.yml', 'r'))
+releases = yaml.load(open('_data/music-picks/2018.yml', 'r'))
 for release in releases:
-    if 'spotify' in release:
+    if 'spotify' not in release:
         print(release['artist'])
         print(release['title'])
-        results = sp.search(q=release['title'],type='album', limit=20)
-        print(results['albums']['items'][0]['external_urls']['spotify'])
-        print('\n')
-        time.sleep(0.5)
+        try:
+            results = sp.search(q=release['title'],type='album', limit=20)
+            url = results['albums']['items'][0]['external_urls']['spotify']
+            print(url)
+            release['spotify'] = url
+        except:
+            print('not found')
+        finally:
+            print('\n')
+            time.sleep(0.5)
+
+with open('_data/music-picks/2018.yml', 'w') as outfile:
+    yaml.dump(releases, outfile, default_flow_style=False)
